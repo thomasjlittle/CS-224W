6f0c92c4868aec3fe89c6d155835db23c9886fe1
Thom <littlethomas98@gmail.com>
2023-03-20 16:44:22 -0700

    Update data_preprocessors.py

--- src/data_preprocessors.py
+++ src/data_preprocessors.py
@@ -126,188 +126,188 @@ def label_fxn_groups(bigsmiles, graphs):
 
 ################################################### Start Maybe Unused #####################################################
 
-# import packages
-# general tools
-import numpy as np
+# # import packages
+# # general tools
+# import numpy as np
 
-# RDkit
-from rdkit import Chem
-from rdkit.Chem.rdmolops import GetAdjacencyMatrix
+# # RDkit
+# from rdkit import Chem
+# from rdkit.Chem.rdmolops import GetAdjacencyMatrix
 
-# Pytorch and Pytorch Geometric
-import torch
-from torch_geometric.data import Data
-from torch.utils.data import DataLoader
-
-
-def one_hot_encoding(x, permitted_list):
-    """
-    Maps input elements x which are not in the permitted list to the last element
-    of the permitted list.
-    """
-    if x not in permitted_list:
-        x = permitted_list[-1]
-    binary_encoding = [
-        int(boolean_value) for boolean_value in list(map(lambda s: x == s, permitted_list))
-    ]
-    return binary_encoding
+# # Pytorch and Pytorch Geometric
+# import torch
+# from torch_geometric.data import Data
+# from torch.utils.data import DataLoader
 
 
-def get_atom_features(atom, use_chirality=False, hydrogens_implicit=False):
-    """
-    Takes an RDKit atom object as input and gives a 1d-numpy array of atom features as output.
-    """
-    # define list of permitted atoms
-
-    permitted_list_of_atoms = [
-        "C",
-        "N",
-        "O",
-        "S",
-        "F",
-        "Si",
-        "Cl",
-        "Br",
-        "Unknown",
-    ]
-
-    if hydrogens_implicit == False:
-        permitted_list_of_atoms = ["H"] + permitted_list_of_atoms
-
-    # compute atom features
-
-    atom_type_enc = one_hot_encoding(str(atom.GetSymbol()), permitted_list_of_atoms)
-
-    n_heavy_neighbors_enc = one_hot_encoding(
-        int(atom.GetDegree()), [0, 1, 2, 3, 4, "MoreThanFour"]
-    )
-
-    formal_charge_enc = one_hot_encoding(
-        int(atom.GetFormalCharge()), [-3, -2, -1, 0, 1, 2, 3, "Extreme"]
-    )
-
-    hybridisation_type_enc = one_hot_encoding(
-        str(atom.GetHybridization()), ["S", "SP", "SP2", "SP3", "SP3D", "SP3D2", "OTHER"]
-    )
-
-    is_in_a_ring_enc = [int(atom.IsInRing())]
-
-    is_aromatic_enc = [int(atom.GetIsAromatic())]
-
-    atomic_mass_scaled = [float((atom.GetMass() - 10.812) / 116.092)]
-
-    vdw_radius_scaled = [float((Chem.GetPeriodicTable().GetRvdw(atom.GetAtomicNum()) - 1.5) / 0.6)]
-
-    covalent_radius_scaled = [
-        float((Chem.GetPeriodicTable().GetRcovalent(atom.GetAtomicNum()) - 0.64) / 0.76)
-    ]
-    atom_feature_vector = (
-        atom_type_enc
-        + n_heavy_neighbors_enc
-        + formal_charge_enc
-        + hybridisation_type_enc
-        + is_in_a_ring_enc
-        + is_aromatic_enc
-        + atomic_mass_scaled
-        + vdw_radius_scaled
-        + covalent_radius_scaled
-    )
-
-    if use_chirality == True:
-        chirality_type_enc = one_hot_encoding(
-            str(atom.GetChiralTag()),
-            ["CHI_UNSPECIFIED", "CHI_TETRAHEDRAL_CW", "CHI_TETRAHEDRAL_CCW", "CHI_OTHER"],
-        )
-        atom_feature_vector += chirality_type_enc
-
-    if hydrogens_implicit == True:
-        n_hydrogens_enc = one_hot_encoding(
-            int(atom.GetTotalNumHs()), [0, 1, 2, 3, 4, "MoreThanFour"]
-        )
-        atom_feature_vector += n_hydrogens_enc
-
-    # Add in_functional_group
-    atom_feature_vector += [0]
-
-    return np.array(atom_feature_vector)
+# def one_hot_encoding(x, permitted_list):
+#     """
+#     Maps input elements x which are not in the permitted list to the last element
+#     of the permitted list.
+#     """
+#     if x not in permitted_list:
+#         x = permitted_list[-1]
+#     binary_encoding = [
+#         int(boolean_value) for boolean_value in list(map(lambda s: x == s, permitted_list))
+#     ]
+#     return binary_encoding
 
 
-def get_bond_features(bond, use_stereochemistry=True):
-    """
-    Takes an RDKit bond object as input and gives a 1d-numpy array of bond features as output.
-    """
-    permitted_list_of_bond_types = [
-        Chem.rdchem.BondType.SINGLE,
-        Chem.rdchem.BondType.DOUBLE,
-        Chem.rdchem.BondType.TRIPLE,
-        Chem.rdchem.BondType.AROMATIC,
-    ]
-    bond_type_enc = one_hot_encoding(bond.GetBondType(), permitted_list_of_bond_types)
+# def get_atom_features(atom, use_chirality=False, hydrogens_implicit=False):
+#     """
+#     Takes an RDKit atom object as input and gives a 1d-numpy array of atom features as output.
+#     """
+#     # define list of permitted atoms
 
-    bond_is_conj_enc = [int(bond.GetIsConjugated())]
+#     permitted_list_of_atoms = [
+#         "C",
+#         "N",
+#         "O",
+#         "S",
+#         "F",
+#         "Si",
+#         "Cl",
+#         "Br",
+#         "Unknown",
+#     ]
 
-    bond_is_in_ring_enc = [int(bond.IsInRing())]
+#     if hydrogens_implicit == False:
+#         permitted_list_of_atoms = ["H"] + permitted_list_of_atoms
 
-    bond_feature_vector = bond_type_enc + bond_is_conj_enc + bond_is_in_ring_enc
+#     # compute atom features
 
-    if use_stereochemistry == True:
-        stereo_type_enc = one_hot_encoding(
-            str(bond.GetStereo()), ["STEREOZ", "STEREOE", "STEREOANY", "STEREONONE"]
-        )
-        bond_feature_vector += stereo_type_enc
-    return np.array(bond_feature_vector)
+#     atom_type_enc = one_hot_encoding(str(atom.GetSymbol()), permitted_list_of_atoms)
+
+#     n_heavy_neighbors_enc = one_hot_encoding(
+#         int(atom.GetDegree()), [0, 1, 2, 3, 4, "MoreThanFour"]
+#     )
+
+#     formal_charge_enc = one_hot_encoding(
+#         int(atom.GetFormalCharge()), [-3, -2, -1, 0, 1, 2, 3, "Extreme"]
+#     )
+
+#     hybridisation_type_enc = one_hot_encoding(
+#         str(atom.GetHybridization()), ["S", "SP", "SP2", "SP3", "SP3D", "SP3D2", "OTHER"]
+#     )
+
+#     is_in_a_ring_enc = [int(atom.IsInRing())]
+
+#     is_aromatic_enc = [int(atom.GetIsAromatic())]
+
+#     atomic_mass_scaled = [float((atom.GetMass() - 10.812) / 116.092)]
+
+#     vdw_radius_scaled = [float((Chem.GetPeriodicTable().GetRvdw(atom.GetAtomicNum()) - 1.5) / 0.6)]
+
+#     covalent_radius_scaled = [
+#         float((Chem.GetPeriodicTable().GetRcovalent(atom.GetAtomicNum()) - 0.64) / 0.76)
+#     ]
+#     atom_feature_vector = (
+#         atom_type_enc
+#         + n_heavy_neighbors_enc
+#         + formal_charge_enc
+#         + hybridisation_type_enc
+#         + is_in_a_ring_enc
+#         + is_aromatic_enc
+#         + atomic_mass_scaled
+#         + vdw_radius_scaled
+#         + covalent_radius_scaled
+#     )
+
+#     if use_chirality == True:
+#         chirality_type_enc = one_hot_encoding(
+#             str(atom.GetChiralTag()),
+#             ["CHI_UNSPECIFIED", "CHI_TETRAHEDRAL_CW", "CHI_TETRAHEDRAL_CCW", "CHI_OTHER"],
+#         )
+#         atom_feature_vector += chirality_type_enc
+
+#     if hydrogens_implicit == True:
+#         n_hydrogens_enc = one_hot_encoding(
+#             int(atom.GetTotalNumHs()), [0, 1, 2, 3, 4, "MoreThanFour"]
+#         )
+#         atom_feature_vector += n_hydrogens_enc
+
+#     # Add in_functional_group
+#     atom_feature_vector += [0]
+
+#     return np.array(atom_feature_vector)
 
 
-def create_pytorch_geometric_graph_data_list_from_smiles_and_labels(x_smiles, y):
-    """
-    Inputs:
+# def get_bond_features(bond, use_stereochemistry=True):
+#     """
+#     Takes an RDKit bond object as input and gives a 1d-numpy array of bond features as output.
+#     """
+#     permitted_list_of_bond_types = [
+#         Chem.rdchem.BondType.SINGLE,
+#         Chem.rdchem.BondType.DOUBLE,
+#         Chem.rdchem.BondType.TRIPLE,
+#         Chem.rdchem.BondType.AROMATIC,
+#     ]
+#     bond_type_enc = one_hot_encoding(bond.GetBondType(), permitted_list_of_bond_types)
 
-    x_smiles = [smiles_1, smiles_2, ....] ... a list of SMILES strings
-    y = [y_1, y_2, ...] ... a list of numerial labels for the SMILES strings (such as associated pKi values)
+#     bond_is_conj_enc = [int(bond.GetIsConjugated())]
 
-    Outputs:
+#     bond_is_in_ring_enc = [int(bond.IsInRing())]
 
-    data_list = [G_1, G_2, ...] ... a list of torch_geometric.data.Data objects which represent labeled molecular graphs that can readily be used for machine learning
+#     bond_feature_vector = bond_type_enc + bond_is_conj_enc + bond_is_in_ring_enc
 
-    """
+#     if use_stereochemistry == True:
+#         stereo_type_enc = one_hot_encoding(
+#             str(bond.GetStereo()), ["STEREOZ", "STEREOE", "STEREOANY", "STEREONONE"]
+#         )
+#         bond_feature_vector += stereo_type_enc
+#     return np.array(bond_feature_vector)
 
-    data_list = []
 
-    for smiles, y_val in zip(x_smiles, y):
-        # convert SMILES to RDKit mol object
-        mol = Chem.MolFromSmiles(smiles)
-        # get feature dimensions
-        n_nodes = mol.GetNumAtoms()
-        n_edges = 2 * mol.GetNumBonds()
-        n_node_features = len(get_atom_features(mol.GetAtomWithIdx(0)))
-        n_edge_features = len(get_bond_features(mol.GetBondBetweenAtoms(0, 1)))
-        # construct node feature matrix X of shape (n_nodes, n_node_features)
-        X = np.zeros((n_nodes, n_node_features))
-        for atom in mol.GetAtoms():
-            X[atom.GetIdx(), :] = get_atom_features(atom)
+# def create_pytorch_geometric_graph_data_list_from_smiles_and_labels(x_smiles, y):
+#     """
+#     Inputs:
 
-        X = torch.tensor(X, dtype=torch.float)
+#     x_smiles = [smiles_1, smiles_2, ....] ... a list of SMILES strings
+#     y = [y_1, y_2, ...] ... a list of numerial labels for the SMILES strings (such as associated pKi values)
 
-        # construct edge index array E of shape (2, n_edges)
-        (rows, cols) = np.nonzero(GetAdjacencyMatrix(mol))
-        torch_rows = torch.from_numpy(rows.astype(np.int64)).to(torch.long)
-        torch_cols = torch.from_numpy(cols.astype(np.int64)).to(torch.long)
-        E = torch.stack([torch_rows, torch_cols], dim=0)
+#     Outputs:
 
-        # construct edge feature array EF of shape (n_edges, n_edge_features)
-        EF = np.zeros((n_edges, n_edge_features))
+#     data_list = [G_1, G_2, ...] ... a list of torch_geometric.data.Data objects which represent labeled molecular graphs that can readily be used for machine learning
 
-        for k, (i, j) in enumerate(zip(rows, cols)):
-            EF[k] = get_bond_features(mol.GetBondBetweenAtoms(int(i), int(j)))
+#     """
 
-        EF = torch.tensor(EF, dtype=torch.float)
+#     data_list = []
 
-        # construct label tensor
-        y_tensor = torch.tensor(np.array([y_val]), dtype=torch.float)
+#     for smiles, y_val in zip(x_smiles, y):
+#         # convert SMILES to RDKit mol object
+#         mol = Chem.MolFromSmiles(smiles)
+#         # get feature dimensions
+#         n_nodes = mol.GetNumAtoms()
+#         n_edges = 2 * mol.GetNumBonds()
+#         n_node_features = len(get_atom_features(mol.GetAtomWithIdx(0)))
+#         n_edge_features = len(get_bond_features(mol.GetBondBetweenAtoms(0, 1)))
+#         # construct node feature matrix X of shape (n_nodes, n_node_features)
+#         X = np.zeros((n_nodes, n_node_features))
+#         for atom in mol.GetAtoms():
+#             X[atom.GetIdx(), :] = get_atom_features(atom)
 
-        # construct Pytorch Geometric data object and append to data list
-        data_list.append(Data(x=X, edge_index=E, edge_attr=EF, y=y_tensor))
-    return data_list
+#         X = torch.tensor(X, dtype=torch.float)
+
+#         # construct edge index array E of shape (2, n_edges)
+#         (rows, cols) = np.nonzero(GetAdjacencyMatrix(mol))
+#         torch_rows = torch.from_numpy(rows.astype(np.int64)).to(torch.long)
+#         torch_cols = torch.from_numpy(cols.astype(np.int64)).to(torch.long)
+#         E = torch.stack([torch_rows, torch_cols], dim=0)
+
+#         # construct edge feature array EF of shape (n_edges, n_edge_features)
+#         EF = np.zeros((n_edges, n_edge_features))
+
+#         for k, (i, j) in enumerate(zip(rows, cols)):
+#             EF[k] = get_bond_features(mol.GetBondBetweenAtoms(int(i), int(j)))
+
+#         EF = torch.tensor(EF, dtype=torch.float)
+
+#         # construct label tensor
+#         y_tensor = torch.tensor(np.array([y_val]), dtype=torch.float)
+
+#         # construct Pytorch Geometric data object and append to data list
+#         data_list.append(Data(x=X, edge_index=E, edge_attr=EF, y=y_tensor))
+#     return data_list
 
 
 ################################################### End Maybe Unused #####################################################
